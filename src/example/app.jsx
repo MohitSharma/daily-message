ec2998a50580cecd2dadcab6816eeee7db1442e6
Mohit Sharma <developer.pht@gmail.com>
2016-04-08 18:30:57 +0530

    Update Example

--- src/example/app.jsx
+++ src/example/app.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import DailyMessage from '../components/daily-message/index.jsx';
 
-const App = React.createClass({
+class App extends React.Component {
   render () {
     return (
       <DailyMessage customMessage="Welcome Mohit Sharma" customClass="welcome" />
     );
   }
 
-});
+}
 
 React.render(
   <App />,
